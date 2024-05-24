f3b340f92a117a480be9a3eb48f351b75b347a89
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-05-24 15:16:40 +0100

    updating App.tsx to view everything on local server

--- challenge/App.tsx
+++ challenge/App.tsx
@@ -1,10 +1,13 @@
 import Greeting from './Greeting.tsx';
 import Shouter from './Shouter.tsx';
+import MouseTracker from './MouseTracker.tsx';
 
 function App() {
 	return (
 		<main>
 			<Greeting name='Halimah' />
+			<Shouter />
+			<MouseTracker />
 		</main>
 	);
 }
