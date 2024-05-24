04496c94d4d30a96f2b05ad1adf5fe958fe66d07
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-05-24 15:10:31 +0100

    solved challenge 3 mouse tracking

--- challenge/MouseTracker.tsx
+++ challenge/MouseTracker.tsx
@@ -1,8 +1,22 @@
+import { useState, useEffect } from 'react';
+
 export default function MouseTracker() {
-	// mousemove event listener needed
+	const [startPoint, setCoords] = useState([0, 0]);
+
+	useEffect(() => {
+		function updateCoords(event: MouseEvent) {
+			event.preventDefault();
+			const x: number = event.clientX;
+			const y: number = event.clientY;
+			setCoords([x, y]);
+		}
+		window.addEventListener('mousemove', updateCoords);
+		return () => window.removeEventListener('mousemove', updateCoords);
+	}, []);
+
 	return (
 		<output>
-			{x},{y}
+			{startPoint[0]},{startPoint[1]}
 		</output>
 	);
 }
